fcaa7a2e924e1468573b1a3a0000ba36b5f3ac0d
johjan Martinez <jebaquer@bancolombia.com.co>
2025-04-12 13:59:29 -0500

    Adición de pruebas

--- prueba/src/test/java/com/appgate/prueba/service/SubsequenceServiceTest.java
+++ prueba/src/test/java/com/appgate/prueba/service/SubsequenceServiceTest.java
@@ -1,4 +1,47 @@
 package com.appgate.prueba.service;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class SubsequenceServiceTest {
+
+    private final SubsequenceService service = new SubsequenceService();
+
+    @Test
+    void testSameStrings() {
+        assertEquals(1, service.getCount("rabbit", "rabbit"));
+    }
+
+    @Test
+    void testExample1() {
+        assertEquals(3, service.getCount("rabbbit", "rabbit"));
+    }
+
+    @Test
+    void testExample2() {
+        assertEquals(5, service.getCount("babgbag", "bag"));
+    }
+
+    @Test
+    void testNoPossibleSubsequence() {
+        assertEquals(0, service.getCount("abc", "d"));
+    }
+
+    @Test
+    void testEmptyT() {
+        assertEquals(1, service.getCount("abc", ""));
+    }
+
+    @Test
+    void testEmptyS() {
+        assertEquals(0, service.getCount("", "a"));
+    }
+
+    @Test
+    void testNullInputs() {
+        assertThrows(IllegalArgumentException.class, () -> service.getCount(null, "a"));
+        assertThrows(IllegalArgumentException.class, () -> service.getCount("a", null));
+    }
 }
