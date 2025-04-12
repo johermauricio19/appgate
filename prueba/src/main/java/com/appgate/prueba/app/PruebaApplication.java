3cf16c81c9bd4160226409f65f1bb00852a68974
johjan Martinez <jebaquer@bancolombia.com.co>
2025-04-12 13:38:14 -0500

    Adición de logica

--- prueba/src/main/java/com/appgate/prueba/app/PruebaApplication.java
+++ prueba/src/main/java/com/appgate/prueba/app/PruebaApplication.java
@@ -1,5 +1,6 @@
 package com.appgate.prueba.app;
 
+import com.appgate.prueba.service.SubsequenceService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PruebaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(PruebaApplication.class, args);
+        SubsequenceService service = new SubsequenceService();
+        System.out.println("Resultado 1: " + service.getCount("rabbbit", "rabbit")); // 3
+        System.out.println("Resultado 2: " + service.getCount("babgbag", "bag"));    // 5
     }
-
 }
